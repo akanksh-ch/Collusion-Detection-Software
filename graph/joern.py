a9bd0bf7f3e675e96a9087e79be8d5177a221880
Akanksh Chitimalla <144524423+akanksh-ch@users.noreply.github.com>
2026-08-06 14:37:22 +0100

    Implemented generate_cpg

--- graph/joern.py
+++ graph/joern.py
@@ -3,15 +3,20 @@ This file contains code to generate CPGs using Joern
 """
 
 import subprocess
+from 
 
 # joern-parse -J-Xmx2048M student01-non-plagiarized.java -o cpg.bin
 # joern-export -J-Xmx2048M --repr cpg cpg.bin -o test
 
-def parse(path: str):
-    pass
+def generate_cpg(path: str):
+    parse_result = subprocess.run(
+            ['joern-parse', '-J-Xmx2048M', path, '-o', f"{path}.bin"],
+        stderr=subprocess.STDOUT
+        text=True  # Returns strings instead of bytes
+    )
 
-def export(path: str):
-    pass
-
-def generate_cpg():
-    pass
+    export_result = subprocess.run(
+            ['joern-export', '-J-Xmx2048M', '--repr', 'cpg', f"{path}.bin", '-o', f"{path}_export"]
+        stderr=subprocess.STDOUT
+        text=True  # Returns strings instead of bytes
+    )
