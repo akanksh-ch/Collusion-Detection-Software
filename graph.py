4d3ea214ad3919276dcb8ad94bbd65a2a902e3c7
Akanksh Chitimalla <144524423+akanksh-ch@users.noreply.github.com>
2026-08-06 12:18:15 +0100

    added path readability

--- graph.py
+++ graph.py
@@ -2,6 +2,9 @@ import networkx as nx
 from platformdirs import user_cache_dir # Storing CPGs and temp stuff in userdirs
 import subprocess
 from pathlib import Path
+from os import access as perm
+from os import R_OK
+from concurrent.futures import ThreadPoolExecutor
 
 CACHE_DIR = user_cache_dir('cds', ensure_exists=True)
 
@@ -10,3 +13,7 @@ def generate_graph(path: str): # file or directory
     # Checking if path is valid
     if not Path(path).exists():
         raise FileNotFoundError(f"File or Directory not found: {str(Path(path).resolve())}")
+
+    # Checking if it's readable
+    if not perm(Path(path), R_OK):
+        raise PermissionError(f"Could not read: {str(Path(path).resolve())}")
