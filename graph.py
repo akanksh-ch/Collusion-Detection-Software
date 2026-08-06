ee3d8983984f425b84ab6ac1af9ec8dd3a2c7b55
Akanksh Chitimalla <144524423+akanksh-ch@users.noreply.github.com>
2026-08-06 11:36:14 +0100

    Added platformdirs and path validation

---
+++ graph.py
@@ -0,0 +1,12 @@
+import networkx as nx
+from platformdirs import user_cache_dir # Storing CPGs and temp stuff in userdirs
+import subprocess
+from pathlib import Path
+
+CACHE_DIR = user_cache_dir('cds', ensure_exists=True)
+
+def generate_graph(path: str): # file or directory
+    
+    # Checking if path is valid
+    if not Path(path).exists():
+        raise FileNotFoundError(f"File or Directory not found: {str(Path(path).resolve())}")
